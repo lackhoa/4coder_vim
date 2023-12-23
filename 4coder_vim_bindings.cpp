a0e029a1a638481f4ba9e0f01ed864ba70b0b7dd
lackhoa <vodangkhoa873@gmail.com>
2023-12-23 19:25:44 +0200

    allow overwites in the binding

--- 4coder_vim_bindings.cpp
+++ 4coder_vim_bindings.cpp
@@ -3,28 +3,31 @@
 #include "4coder_vim.h"
 
 // TODO(BYP): Might want meta-data on these, for now I prefer the simplicity
-function void vim_map_set_binding(u32 mode, u32 sub_mode, void *func, u64 key) {
+function b32 vim_map_set_binding(u32 mode, u32 sub_mode, void *func, u64 key) {
+  b32 no_conflict = true;
   if ((mode & bitmask_3) == 0) {
     // set everything
     mode |= bitmask_3;
     foreach(s,VIM_SUBMODE_COUNT) {
-      vim_map_set_binding(mode, s, func, key);
+      no_conflict = vim_map_set_binding(mode, s, func, key) && no_conflict;
     }
   } else {
     for (i32 mode_index=0; mode_index < 3; mode_index++) {
       if (mode & (1 << mode_index)) {
-        b32 result = table_insert(vim_maps + mode_index + sub_mode*VIM_MODE_COUNT, key, PtrAsInt(func));
-        AssertAlways(result);  // vim key defined twice
+        Table_u64_u64 *table = vim_maps + mode_index + sub_mode*VIM_MODE_COUNT;
+        no_conflict = !table_erase(table, key);
+        table_insert(table, key, PtrAsInt(func));
       }
     }
   }
+  return no_conflict;
 }
 
-function void VimBind(u32 mode, Custom_Command_Function *custom, Vim_Sub_Mode sub_mode, u64 key){
-	vim_map_set_binding(mode, sub_mode, (void *)custom, key);
+function b32 VimBind(u32 mode, Custom_Command_Function *custom, Vim_Sub_Mode sub_mode, u64 key){
+	return vim_map_set_binding(mode, sub_mode, (void *)custom, key);
 }
-function void VimBind(u32 mode, Custom_Command_Function *custom, u64 key){
-	vim_map_set_binding(mode, SUB_None, (void *)custom, key);
+function b32 VimBind(u32 mode, Custom_Command_Function *custom, u64 key){
+	return vim_map_set_binding(mode, SUB_None, (void *)custom, key);
 }
 
 /*
